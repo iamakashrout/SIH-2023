e1c4ad48ddf76c6751775333cfd3f7da125d19df
NandiniGera <nandini.gera18@gmail.com>
2023-09-18 17:23:46 +0530

    space added

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,14 +1,21 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Header from "../components/header"
-import Categories from '@/components/categories'
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Header from "../components/header";
+import Categories from "@/components/categories";
+import LatestPosts from "@/components/latestPosts";
+import Card from "@/components/card";
+const inter = Inter({ subsets: ["latin"] });
+import ImageGallery from '@/components/ImageCarousel'
 
 export default function Home() {
   return (
     <>
-    <Header/>
-    <Categories/>
+      <Header />
+      <Categories />
+      <ImageGallery/>
+      <div className="m-4 p-2"> 
+    </div>
+      <LatestPosts />
     </>
   );
 }
