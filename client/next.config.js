9bc45639d06b0e5a3dc1a241b23d7f75f71716f1
Ankur Gupta <ankurgupta.bt21cse@pec.edu.in>
2023-09-18 16:59:18 +0530

    Latest Posts

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
