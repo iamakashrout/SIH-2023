6482c3f10b7c3b49ac2aace04f2f4dad2d96251e
iamakashrout <iamakashrout@gmail.com>
2023-09-18 23:26:48 +0530

    category labels fixed

--- client/src/components/categories.tsx
+++ client/src/components/categories.tsx
@@ -4,15 +4,15 @@ const categories = () => {
   return (
     <>
       <div className="flex justify-center items-center space-x-10 pt-3 font-bold bg-gray-50 pb-3">
-        <a className = "hover:cursor-pointer hover:scale-[1.02] duration-300" href="#">LIFESTYLE</a>
-        <a className = "hover:cursor-pointer hover:scale-[1.02] duration-300" href="#">BUSINESS</a>
+        <a className = "hover:cursor-pointer hover:scale-[1.02] duration-300" href="#">ENTERTAINMENT</a>
+        <a className = "hover:cursor-pointer hover:scale-[1.02] duration-300" href="#">JUDICIARY</a>
+        <a className = "hover:cursor-pointer hover:scale-[1.02] duration-300" href="#">SCIENCE</a>
+        <a className = "hover:cursor-pointer hover:scale-[1.02] duration-300" href="#">CRIME</a>
+        <a className = "hover:cursor-pointer hover:scale-[1.02] duration-300" href="#">POLITICS</a>
         <a className = "hover:cursor-pointer hover:scale-[1.02] duration-300" href="#">SPORTS</a>
-        <a className = "hover:cursor-pointer hover:scale-[1.02] duration-300" href="#">FASHION</a>
-        <a className = "hover:cursor-pointer hover:scale-[1.02] duration-300" href="#">HEALTH</a>
         <a className = "hover:cursor-pointer hover:scale-[1.02] duration-300" href="#">TECHNOLOGY</a>
-        <a className = "hover:cursor-pointer hover:scale-[1.02] duration-300" href="#">TRAVEL</a>
-        <a className = "hover:cursor-pointer hover:scale-[1.02] duration-300" href="#">FOOD</a>
-        <a className = "hover:cursor-pointer hover:scale-[1.02] duration-300" href="#">CREATIVE</a>
+        <a className = "hover:cursor-pointer hover:scale-[1.02] duration-300" href="#">CULTURE</a>
+        <a className = "hover:cursor-pointer hover:scale-[1.02] duration-300" href="#">INTERNATIONAL</a>
       </div>
     </>
   );
